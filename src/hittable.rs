0aa45f98724b6b7356862584bd6f2cb1308530c5
ben-3dninja <benjamin.lagosanto@gmail.com>
2022-05-07 15:49:35 +0200

    Formated file

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,4 +1,7 @@
-use crate::{vec3::{Point3, Vec3}, ray::Ray};
+use crate::{
+    ray::Ray,
+    vec3::{Point3, Vec3},
+};
 
 pub struct HitRecord {
     // The point where the ray hit the object
@@ -12,9 +15,9 @@ pub struct HitRecord {
 #[allow(unused_variables)]
 pub trait Hittable {
     /**
-     This function returns true if the ray hits the object with a t in the desired range, and false otherwise.
-     In addition, it modifies the HitRecord accordingly.
-     */
+    This function returns true if the ray hits the object with a t in the desired range, and false otherwise.
+    In addition, it modifies the HitRecord accordingly.
+    */
     fn hit(&self, ray: Ray, t_min: f64, t_max: f64, record: &mut HitRecord) -> bool {
         false
     }
