3cafe55b237f0fbefdb0175196dd4bfc10fac9b7
ben3dninja <benjamin.lagosanto@gmail.com>
2022-05-08 11:51:50 +0200

    Refactored hit and added test

--- src/hittable_list.rs
+++ src/hittable_list.rs
@@ -17,18 +17,40 @@ impl HittableList {
     pub fn add(&mut self, object: Rc<dyn Hittable>) {
         self.objects.push(object);
     }
-
+    /**
+     This function iterates through all the objects to find the closest one to the camera
+     that the ray hits. It then returns the record of that hit.
+     */
     pub fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
         let mut rec = None;
         let mut closest_so_far = t_max;
 
         for object in self.objects.iter() {
-            let temp_rec = object.hit(&ray, t_min, closest_so_far);
-            if temp_rec.is_some() {
-                closest_so_far = temp_rec.as_ref().unwrap().t;
-                rec = temp_rec;
+            if let Some(temp_rec ) = object.hit(&ray, t_min, closest_so_far) {
+                closest_so_far = temp_rec.t;
+                rec = Some(temp_rec);
             }
         }
         rec
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::rc::Rc;
+
+    use crate::{sphere::Sphere, vec3::{Point3, Vec3}, ray::Ray};
+
+    use super::HittableList;
+
+    #[test]
+    fn closest_record() {
+        let sphere1 = Sphere::new(Point3::new(-2.0, 0.0, 0.0), 0.5);
+        let sphere2 = Sphere::new(Point3::new(-4.0, 0.0, 0.0), 0.5);
+        let ray = Ray::new(Point3::new(0.0, 0.0, 0.0), Vec3::new(-1.0, 0.0, 0.0));
+        let mut world = HittableList::empty();
+        world.add(Rc::new(sphere1));
+        world.add(Rc::new(sphere2));
+        assert_eq!(1.5, world.hit(&ray, 0.0, 10.0).unwrap().t);
     }
 }
