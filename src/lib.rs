00a14fc89d61c231240fc0e2600b331c723b5b64
ben3dninja <benjamin.lagosanto@gmail.com>
2022-05-10 21:50:21 +0200

    Renamed HittableList as World and objects as hitttables for clarity

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
+pub mod camera;
 pub mod hittable;
-pub mod hittable_list;
 pub mod ray;
 pub mod sphere;
 pub mod vec3;
-pub mod camera;
+pub mod world;
 
 #[macro_use]
 extern crate impl_ops;
--- src/world.rs
+++ src/world.rs
@@ -5,31 +5,31 @@ use crate::{
     ray::Ray,
 };
 
-pub struct HittableList {
-    objects: Vec<Rc<dyn Hittable>>,
+pub struct World {
+    hittables: Vec<Rc<dyn Hittable>>,
 }
 
 #[allow(dead_code)]
-impl HittableList {
+impl World {
     pub fn empty() -> Self {
         Self {
-            objects: Vec::new(),
+            hittables: Vec::new(),
         }
     }
 
-    pub fn add(&mut self, object: Rc<dyn Hittable>) {
-        self.objects.push(object);
+    pub fn add(&mut self, hittable: Rc<dyn Hittable>) {
+        self.hittables.push(hittable);
     }
     /**
-    This function iterates through all the objects to find the closest one to the camera
+    This function iterates through all the hittables to find the closest one to the camera
     that the ray hits. It then returns the record of that hit.
     */
     pub fn trace_ray(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
         let mut rec = None;
         let mut closest_so_far = t_max;
 
-        for object in self.objects.iter() {
-            if let Some(temp_rec) = object.hit(&ray, t_min, closest_so_far) {
+        for hittable in self.hittables.iter() {
+            if let Some(temp_rec) = hittable.hit(&ray, t_min, closest_so_far) {
                 closest_so_far = temp_rec.t;
                 rec = Some(temp_rec);
             }
@@ -44,14 +44,14 @@ mod test {
 
     use crate::{ray::Ray, sphere::Sphere};
 
-    use super::HittableList;
+    use super::World;
 
     #[test]
     fn closest_record() {
         let sphere1 = Sphere::ni(-2, 0, 0, 0.5);
         let sphere2 = Sphere::ni(-4, 0, 0, 0.5);
         let ray = Ray::ni(0, 0, 0, -1, 0, 0);
-        let mut world = HittableList::empty();
+        let mut world = World::empty();
         world.add(Rc::new(sphere1));
         world.add(Rc::new(sphere2));
         assert_eq!(1.5, world.trace_ray(&ray, 0.0, 10.0).unwrap().t);
